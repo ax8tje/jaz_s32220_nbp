0fa70350d768b76bb009a18212bf083388996c1a
ax8tje <131400863+ax8tje@users.noreply.github.com>
2025-06-21 12:46:55 +0200

    Initial commit

---
+++ src/main/java/pl/pjatk/jaz_s32220_nbp/JazS32220NbpApplication.java
@@ -0,0 +1,13 @@
+package pl.pjatk.jaz_s32220_nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazS32220NbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JazS32220NbpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/pjatk/jaz_s32220_nbp/JazS32220NbpApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pjatk.jaz_s32220_nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JazS32220NbpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
